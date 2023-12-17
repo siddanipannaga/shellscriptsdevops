7ccb37b28a9998ae6e1958e798849a5a0fa70892
siddanipannaga <siddanipannaga24@gmail.com>
2023-12-17 17:38:24 +0530

    new added 3rd time

--- 15-install-packages.sh
+++ 15-install-packages.sh
@@ -3,7 +3,18 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0- $TIMESTAMP.log"
+echo "script started executing at $TIMESTAMP" &>> LOGFILE
+VALIDATE (){
+   then
+      echo -e "$2... $R failed $N "   
+   else
+      echo -e "$2...$R sucess $N "
+      fi
+}
 if [ $ID -ne 0 ]
 then
    echo -e  "$R ERROR: Please run this script with root user $N"
@@ -11,5 +22,17 @@ then
 else
    echo "You are a root user"
 fi
-echo "all arguments passed $@"
+# echo "all arguments passed $@"
+
+for package in $0
+do
+   yum list installed $package
+   if [  $? -ne 0]
+   then
+   yum install $package -y
+   VALIDATE $? "Installation of package"
+   else
+      echo -e "$package is already installed....$Y skipping $N"
+   fi
+done 
 
