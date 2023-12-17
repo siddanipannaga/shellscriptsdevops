1167d4ee4406cb37a121f4ea9108e98328a9d478
siddanipannaga <siddanipannaga24@gmail.com>
2023-12-17 17:58:13 +0530

    new added 3rd time

--- 15-install-packages.sh
+++ 15-install-packages.sh
@@ -9,7 +9,7 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0- $TIMESTAMP.log"
 echo "script started executing at $TIMESTAMP" &>> LOGFILE
 VALIDATE (){
-   if [ $1 -ne 0]
+   if [ $1 -ne 0 ]
    then
       echo -e "$2... $R failed $N "   
    else
