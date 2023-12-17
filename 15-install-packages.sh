50498166642ff3c08529bd314ca15a89ed394d56
siddanipannaga <siddanipannaga24@gmail.com>
2023-12-17 14:59:52 +0530

    added new scripts 2

--- 15-install-packages.sh
+++ 15-install-packages.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 N="\e[0m"
 if [ $ID -ne 0 ]
 then
-   echo -e " $R ERROR: Please run this script with root user $N"
+   echo -e  "$R ERROR: Please run this script with root user $N"
    exit 1 # 
 else
    echo "You are a root user"
