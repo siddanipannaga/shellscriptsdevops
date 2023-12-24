bd9f040ca06b9ae1e4494c553061b35c0c37ef88
siddanipannaga <siddanipannaga24@gmail.com>
2023-12-24 11:35:59 +0530

    delete old fles log added

--- 17-delete-old-log.sh
+++ 17-delete-old-log.sh
@@ -10,7 +10,7 @@ if [ ! -d $SOURCE_DIR ] #! denotes opposite not exist ! -d indicated the diracto
 then    
     echo -e " $R source directory: $SOURCE_DIR does not exit : $N "
 fi
-FILES_TO_DELETE= $(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line
 do 
