246774dab16ec432753f5d5e99bcf52c00b03070
siddanipannaga <siddanipannaga24@gmail.com>
2023-12-25 09:20:11 +0530

    added ifs

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -8,8 +8,9 @@ do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1 )
 
 
-done <<<$DISK_USAGE
 if [ $usage -ge $DISK_THRESHOLD ]
     then
         message="High Disk Usage on $partition: $usage <br>"
     fi
+    
+done <<<$DISK_USAGE
